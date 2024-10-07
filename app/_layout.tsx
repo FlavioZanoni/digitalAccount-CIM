142cb6a2a85dcc51a72df567d3bbec0d0ff773be
FlavioZanoni <flaviozg888@gmail.com>
2024-10-07 15:45:48 -0300

    redirect if not logged

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,31 +1,46 @@
 import { DarkTheme, DefaultTheme, ThemeProvider } from '@react-navigation/native';
 import { useFonts } from 'expo-font';
-import { Stack } from 'expo-router';
+import { Stack, router } from 'expo-router';
 import * as SplashScreen from 'expo-splash-screen';
 import { useEffect } from 'react';
 import 'react-native-reanimated';
 import { useColorScheme } from '@/hooks/useColorScheme';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { UserProvider } from '@/hooks/useUserContext';
+import { StorageService } from '@/lib/StorageService';
+import { TOKEN_COOKIE_NAME } from '@/constants';
 
 // Prevent the splash screen from auto-hiding before asset loading is complete.
 SplashScreen.preventAutoHideAsync();
 
 export default function RootLayout() {
   const colorScheme = useColorScheme();
+  const queryClient = new QueryClient()
+
   const [loaded] = useFonts({
     SpaceMono: require('../assets/fonts/SpaceMono-Regular.ttf'),
   });
 
   useEffect(() => {
-    if (loaded) {
-      SplashScreen.hideAsync();
+    checkExistingToken()
+  }, []);
+
+  const checkExistingToken = async () => {
+    const token = await StorageService.getItem(TOKEN_COOKIE_NAME)
+    if (token) {
+      router.push("/(tabs)/")
     }
-  }, [loaded]);
-  const queryClient = new QueryClient()
+  }
+
+  useEffect(() => {
+    if (loaded) {
+      SplashScreen.hideAsync()
+    }
+  }, [loaded])
+
 
   if (!loaded) {
-    return null;
+    return null
   }
 
   return (
