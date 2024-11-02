7a04d8dd2162fd9a6ad23e10495c7ed295ee9e28
FlavioZanoni <flaviozg888@gmail.com>
2024-11-02 19:01:44 -0300

    prod url

--- constants/index.ts
+++ constants/index.ts
@@ -1,6 +1,6 @@
 const USER_COOKIE_NAME = "cimUser"
 const TOKEN_COOKIE_NAME = "cimToken"
-const API_URL = "https://sigillum.com/"
+const API_URL = "https://sigillum.com.br/"
 const ROLES = ["user"] as const
 
 export { USER_COOKIE_NAME, TOKEN_COOKIE_NAME, ROLES, API_URL }
